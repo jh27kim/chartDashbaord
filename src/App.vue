87dfe98292216157f2233a09611cbefdd567986c
jai <jh27kim@gmail.com>
2022-06-01 20:07:51 +0900

    test code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Doughnut :chart-data="chartData" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// DataPage.vue
+import { Doughnut } from 'vue-chartjs'
+// import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+// ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+import {Chart, ArcElement} from 'chart.js'
+import axios from 'axios'
+
+Chart.register(ArcElement);
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { Doughnut },
+  data() {
+    return {
+      chartOptions: {
+        hoverBorderWidth: 20
+      },
+      chartData: {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 10,
+        labels: ["Green", "Red", "Blue"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            data: [1, 10, 5]
+          }
+        ]
+      },
+      methods: {
+        getChartData: function() {
+          axios.get("localhost:8081/chart-data")
+          .then(function(response){
+          console.log(response);
+          this.data = response.data;
+          })
+        }
+      },
+      beforeMount() {
+        this.getChartData()
+      }
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
