7905b944b959c40a4fa8cf9da6c9b7894049acf5
gjw0407 <gjw0407@gmail.com>
2022-07-06 22:46:17 +0900

    fix: apply changes from origin

--- src/components/PieChart.vue
+++ src/components/PieChart.vue
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 <template>
-  <div>
-    <Doughnut :chartData="chartData"></Doughnut>
-=======
-<template> 
   <div v-for="chart in chartinfo" :key="chart">
     <PieChartTempVue :chartinfo="chart"></PieChartTempVue>
     <!-- {{ index }} {{ chart.chartId }} {{ chart.type }} {{ chart.chartdata }} {{ chart.period }} {{ chart.keyword }} {{ chart.enddate }} -->
->>>>>>> de678eb00511dde2c16578f8172eb92225058e92
   </div>
 
   <div>
@@ -18,102 +12,57 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-import { Doughnut } from 'vue-chartjs';
-import axios from 'axios';
-import { Chart, ArcElement } from 'chart.js';
-
-Chart.register(ArcElement);
-=======
 import axios from 'axios';
 import PieChartTempVue from './PieChartTemp.vue';
->>>>>>> de678eb00511dde2c16578f8172eb92225058e92
 
 export default {
   components: { PieChartTempVue },
   data() {
     return {
-<<<<<<< HEAD
-      keyword: 'mama',
-      chartOptions: {
-        hoverBorderWidth: 10,
-      },
-      chartData: {
-        hoverBackgroundColor: 'red',
-        hoverBorderWidth: 5,
-        labels: this.$store.pieLabels,
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: ['#41B883', '#E46651'],
-            data: [1, 3],
-          },
-        ],
-      },
+      chartinfo: [
+        {
+          chartId: 1,
+          type: 'pie',
+          chartdata: [1, 4],
+          period: [3000],
+          keyword: 'trump',
+          enddate: '20220701',
+        },
+        {
+          chartId: 2,
+          type: 'pie',
+          chartdata: [5, 2],
+          period: [5000000],
+          keyword: 'biden',
+          enddate: '20220702',
+        },
+        {
+          chartId: 3,
+          type: 'pie',
+          chartdata: [7, 9],
+          period: [9000000],
+          keyword: 'obama',
+          enddate: '20220703',
+        },
+      ],
     };
   },
-=======
-      chartinfo: [{chartId: 1, type:"pie", chartdata: [1, 4], period: [3000], keyword: "trump", enddate: "20220701"},
-      {chartId: 2, type:"pie", chartdata: [5, 2], period: [5000000], keyword: "biden", enddate: "20220702"},
-      {chartId: 3, type:"pie", chartdata: [7, 9], period: [9000000], keyword: "obama", enddate: "20220703"}]
-      }
-    },
 
->>>>>>> de678eb00511dde2c16578f8172eb92225058e92
   methods: {
     GetChartForm: function () {
       this.$router.push('/chartform');
     },
 
-<<<<<<< HEAD
-    GetChartMetada: function (userId) {
-      axios
-        .get('http://localhost:8082/chart/')
-        .then((response) => {})
-        .catch((error) => {
-          console.log(error);
-        });
+    GetChartMetadata: function () {
+      axios.get('http://localhost:8081/chart/');
+      // Get chart metadata from server
       return;
     },
-
-    GetChartData: function () {
-      setInterval(() => {
-        console.log(this.keyword);
-        axios
-          .get('http://localhost:8082/chart-data/' + this.keyword)
-          .then((response) => {
-            this.pieLabels = [];
-            this.pieData = [];
-            for (var label in response.data.data) {
-              this.pieLabels.push(label);
-              this.pieData.push(response.data.data[label]);
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-            this.pieData = [1, 29];
-          });
-      }, 2000);
-    },
-  },
-
-  mounted() {
-    setInterval(() => this.GetChartData(), 2000);
   },
 };
 </script>
-=======
-    GetChartMetadata: function() {
-      axios.get("http://localhost:8081/chart/")
-      // Get chart metadata from server
-      return;
-    }
-  }
-}
-</script>
 
- 
-  <!-- //   GetChartData: function (){
+<!-- //   GetChartData: function (){
   //         setInterval(() => {
   //            axios.get("http://localhost:8082/chart-data/" + this.keyword)
   //            .then((response) => {
@@ -140,4 +89,3 @@ export default {
   //   setInterval(() => this.GetChartData(), 2000);
   // }
 // } -->
->>>>>>> de678eb00511dde2c16578f8172eb92225058e92
--- src/components/PieChartTemp.vue
+++ src/components/PieChartTemp.vue
@@ -1,136 +1,61 @@
 <template>
-<<<<<<< HEAD
   <div>
     <Doughnut :chartData="chartData"></Doughnut>
-  </div>
-  <div>
-    <button type="button" class="btn btn-info" @click="GetChartForm">
-      New Chart
-    </button>
+    {{ period }}
   </div>
 </template>
 
 <script>
-import { Doughnut } from 'vue-chartjs';
 import axios from 'axios';
 import { Chart, ArcElement } from 'chart.js';
-=======
-    <div>
-        <Doughnut :chartData="chartData"></Doughnut>
-        {{period}}
-    </div>
-</template>
-
-<script>
-
-import axios from 'axios';
-import {Chart, ArcElement} from 'chart.js'
-import { Doughnut } from 'vue-chartjs'
->>>>>>> de678eb00511dde2c16578f8172eb92225058e92
-
+import { Doughnut } from 'vue-chartjs';
 Chart.register(ArcElement);
-
 export default {
   components: { Doughnut },
-<<<<<<< HEAD
+  props: ['chartinfo'],
   data() {
     return {
-      keyword: 'mama',
+      keyword: this.chartinfo.keyword,
+      period: this.chartinfo.period,
       chartOptions: {
-        hoverBorderWidth: 10,
+        hoverBorderWidth: 5,
       },
       chartData: {
         hoverBackgroundColor: 'red',
         hoverBorderWidth: 5,
-        labels: this.$store.pieLabels,
+        labels: ['agree', 'disagree'],
         datasets: [
           {
             label: 'Data One',
             backgroundColor: ['#41B883', '#E46651'],
-            data: [1, 3],
+            data: [1, 1],
           },
         ],
       },
     };
   },
+
   methods: {
     GetChartForm: function () {
       this.$router.push('/chartform');
     },
-
     GetChartData: function () {
       setInterval(() => {
-        console.log(this.keyword);
         axios
           .get('http://localhost:8082/chart-data/' + this.keyword)
           .then((response) => {
-            this.pieLabels = [];
-            this.pieData = [];
-            for (var label in response.data.data) {
-              this.pieLabels.push(label);
-              this.pieData.push(response.data.data[label]);
-            }
+            console.log(response);
+            this.chartData.datasets[0].data = this.data;
           })
           .catch((error) => {
             console.log(error);
-            this.pieData = [1, 29];
+            // this.pieData = [1, 29];
           });
-      }, 2000);
-=======
-  props: ['chartinfo'],
-  data() {
-    return {
-      keyword: this.chartinfo.keyword,
-      period : this.chartinfo.period,
-      chartOptions: {
-        hoverBorderWidth: 5
-      },
-      chartData: {
-        hoverBackgroundColor: "red",
-        hoverBorderWidth: 5,
-        labels: ["agree", "disagree"],
-        datasets: [
-          {
-            label: "Data One",
-            backgroundColor: ["#41B883", "#E46651"],
-            data: [1, 1]
-          }
-        ]
-      },
-    }
-  },
-  
-  methods: {
-    GetChartForm: function() {
-      this.$router.push('/chartform')
-    },
-
-    GetChartData: function (){
-          setInterval(() => {
-             axios.get("http://localhost:8082/chart-data/" + this.keyword)
-             .then((response) => {
-                console.log(response);
-                this.chartData.datasets[0].data = this.data
-            })
-            .catch((error) => {
-                console.log(error);
-                // this.pieData = [1, 29];
-              });
-          }, this.period);
-            
->>>>>>> de678eb00511dde2c16578f8172eb92225058e92
+      }, this.period);
     },
   },
-
   mounted() {
-<<<<<<< HEAD
-    setInterval(() => this.GetChartData(), 2000);
+    setInterval(() => this.GetChartData(), this.period);
   },
 };
 </script>
-=======
-    setInterval(() => this.GetChartData(), this.period);
-  }
-}
-</script>
->>>>>>> de678eb00511dde2c16578f8172eb92225058e92
