1068530a2548a6e43100ee73b0c713d8f10e8d96
jai <jh27kim@gmail.com>
2022-08-08 21:22:04 +0900

    pie chart data axios

--- src/components/PieChartTemp.vue
+++ src/components/PieChartTemp.vue
@@ -24,12 +24,12 @@ export default {
       chartData: {
         hoverBackgroundColor: 'red',
         hoverBorderWidth: 5,
-        labels: [],
+        labels: ["a", "b", "c", "d", "e"],
         datasets: [
           {
             label: 'Data One',
-            backgroundColor: ['#41B883', '#E46651'],
-            data: [0, 0, 0, 0, 0],
+            backgroundColor: ['#41B883', '#E46651','#41B853', '#E16251','#41B883'],
+            data: [1, 2, 3, 4, 5],
           },
         ],
       },
@@ -41,16 +41,15 @@ export default {
       this.$router.push('/chartform');
     },
     getChartData: function (){
+          console.log("initial - "+this.chartData.datasets[0].data)
+          console.log("initial - "+this.chartData.labels)
           setInterval(() => {
              axios.get("http://localhost:8082/chart-data/" + this.keyword)
              .then((response) => {
-                console.log(response);
                 this.chartData.labels = response.data[0].labelList
                 for (let i = 0; i < 5; i++) {
-                  // TODO Increment data 
-                  this.chartData.datasets[0].data[i] += response.data[0].quantityList
+                  this.chartData.datasets[0].data[i] += (response.data[0].quantityList[i])
                 }
-                // this.chartData.datasets[0].data = this.data
             })
             .catch((error) => {
                 console.log(error);
